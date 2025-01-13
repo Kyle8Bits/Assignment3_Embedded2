3dfb2c218752098d6263d044198813527682cc2a
dangkhoa0203 <dcdkhoa@gmail.com>
2025-01-13 17:22:10 +0700

    finalize 1

--- Assignment3_Question2/main.c
+++ Assignment3_Question2/main.c
@@ -9,16 +9,27 @@
 #include "clockState.h"
 
 #define K1_pressed (!(PA->PIN & (1<<2)))
+#define Col2_pressed (!(PA->PIN & (1<<1)))
 #define Col3_pressed (!(PA->PIN & (1<<0)))
+#define Rotate_pressed (!(PB->PIN &(1 << 15)))
 
-volatile int countU13 = 0;
 volatile int countU14 = 0;
+volatile int countU13 = 0;
 volatile int countU12 = 0;
 volatile int countU11 = 0;
 
+volatile int countU14temp = 0;
+volatile int countU13temp = 0;
+volatile int countU12temp = 0;
+volatile int countU11temp = 0;
+
+
 #define MAX_HISTORY 5
 int history[MAX_HISTORY][4] = {0}; // Array to store up to 5 sets of counter values
+volatile 
+	
 int history_index = 0;  
+volatile int display_index = 0;
 
 enum clock_state {START, PAUSE, IDLE, RESET, DISPLAY};
 enum clock_state state = IDLE;
@@ -26,6 +37,9 @@ enum clock_state state = IDLE;
 void TMR1_IRQHandler(void);
 void saveToHistory(void);
 void resetCounters(void);
+void enterDisplayMode(void);
+void exitDisplayMode(void);
+void displayLapsRecord(void);
 
 static void K1_checkpress(){
 	PA->DOUT &= ~(1<<3);
@@ -33,9 +47,9 @@ static void K1_checkpress(){
 	PA->DOUT |= (1<<5);
 	
 	if(K1_pressed){
-		if(state == START){
+		if(state == START && state != DISPLAY){
 			state = PAUSE;
-		}else{
+		}else if(state != START && state != DISPLAY){
 			state = START;
 		}
 	}
@@ -47,9 +61,42 @@ static void K9_checkpress(){
 	PA->DOUT &= ~(1<<5);
 	
 	if(Col3_pressed){
-		state = RESET; 
+		if(state == PAUSE){
+			state = RESET; 
+		}
 	}
 }
+
+static void K5_checkpress(){
+	PA->DOUT |= (1<<3);
+	PA->DOUT &= ~(1<<4);
+	PA->DOUT |= (1<<5);
+	
+	if(Col2_pressed){
+		if(state == PAUSE||state == IDLE){
+			enterDisplayMode();
+			state = DISPLAY;
+		}
+		else{
+			exitDisplayMode();
+			state = PAUSE;
+		}
+	}
+}
+
+
+static void Rotate_checkpress(){
+	if(display_index < 4 ){
+		display_index+=1;
+	}
+	else{
+		display_index = 0;
+	}
+		displayLapsRecord();
+		PC->DOUT ^= (1<<15);
+}
+
+
 
 void checkStateForClock(){
 	switch (state){
@@ -72,7 +119,10 @@ void checkStateForClock(){
 			state = IDLE; // Transition to IDLE after resetting
 			break;
 		case DISPLAY:
-			
+			PC->DOUT |= (1<<14);
+			PC->DOUT |= (1<<13);
+			PC->DOUT |= (1<<12);
+			pauseCount();
 			break;
 		default:
 			PC->DOUT |= (1<<14);
@@ -107,6 +157,27 @@ void resetCounters() {
     showNumbers(countU11, countU12, countU13, countU14);
 }
 
+void displayLapsRecord(){
+		countU11= display_index+1; // Assign history value for U11
+		countU12= history[display_index][1]; // Assign history value for U12
+		countU13= history[display_index][2]; // Assign history value for U13
+		countU14= history[display_index][3]; // Assign history value for U14
+}
+
+void enterDisplayMode(){
+	countU11temp = countU11;
+	countU12temp = countU12;
+	countU13temp = countU13;
+	countU14temp = countU14;
+	
+	displayLapsRecord();
+}
+void exitDisplayMode(){
+	countU11 = countU11temp;
+	countU12 = countU12temp;
+	countU13 = countU13temp;
+	countU14 = countU14temp;
+}
 
 int main(void)
 {
@@ -142,6 +213,14 @@ int main(void)
 			K9_checkpress(); // Transition to RESET state
 			while (Col3_pressed); // Debounce the button
 		}	
+		if(Col2_pressed){
+			K5_checkpress();
+			while(Col2_pressed);
+		}
+		if(Rotate_pressed){
+			Rotate_checkpress();
+			while(Rotate_pressed);
+		}
 	}
 }
 
--- Assignment3_Question2/setupGPIO.h
+++ Assignment3_Question2/setupGPIO.h
@@ -5,12 +5,17 @@
 
 void setupGPIO(){
 	// --START GPIO INITIALIZATION--
-	PC->PMD &= ~(0xFF << 8);
-	PC->PMD |= (0b01010101 << 8);
+	
+	PC->PMD &= ~(0xFF << 10);
+	PC->PMD |= (0b01010101 << 10);
 	
 	PC->PMD &= ~(0x3 << 28);
 	PC->PMD |= (0x1 << 28);
 	
 	PE->PMD &= ~(0xFFFF << 0);
 	PE->PMD |= 0b0101010101010101<<0; 
+	
+	PB->PMD &= ~(0b11<<30);
+	PB->IEN |= (1<<15);
+
 }
